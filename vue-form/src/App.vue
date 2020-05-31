eb70a564d3070ebb4077a376022a98664feb9fd8
tkakcy159 <tkakcy159@gmail.com>
2020-05-31 16:06:22 +0900

    age-of-vuejs final

---
+++ vue-form/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input type="password" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data: fu nction () {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // event.preventDefault();
+
+      console.log(this.username);
+      console.log(this.password);
+      
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password
+      }
+
+      axios.post(url, data)
+        .then(function(res) {
+          console.log(res);
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
